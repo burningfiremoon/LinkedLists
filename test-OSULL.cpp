18bbbaee9ddbe8becd8fdb12c0ef4300cb1b1ebd
burningfiremoon <charleszhang4@gmail.com>
2024-03-12 16:48:03 -0700

    created skeleton to test OSULL.h

---
+++ test-OSULL.cpp
@@ -0,0 +1,14 @@
+#include "OSULL.h"
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    OSULL<int> test;
+
+    test.display();
+    cout << test.getMin();
+    cout << endl << test.getMax();
+
+    return 0;
+}
